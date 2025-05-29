55cc356c8e8f01e19af35d05e6c06e5a3b1b32ba
Vladislaw1 <vladzajcenko077@gmail.com>
2025-05-29 18:06:54 +0300

    added logo

--- docs/assets/mock/teams.js
+++ docs/assets/mock/teams.js
@@ -1,30 +1,18 @@
 export let Teams;
 Teams = [{
     name: 'ЦА ДБР',
-    logo: 'assets/images/teams/logo/CA.png',
+    logo: 'assets/images/teams/logo/2_120х137_-removebg-preview.png',
     id: 1
 }, {
     name: 'ТУ КИЇВ',
-    logo: 'assets/images/teams/logo/Kyev.png',
+    logo: 'assets/images/teams/logo/4_120х137_-Київ-removebg-preview.png',
     id: 2
-}, {
-    name: 'ТУ ЛЬВІВ',
-    logo: 'assets/images/teams/logo/lviv.png',
-    id: 3
-}, {
-    name: 'ТУ МЕЛІТОПОЛЬ',
-    logo: 'assets/images/teams/logo/melitopl.png',
-    id: 4
-}, {
-    name: 'ТУ ПОЛТАВА',
-    logo: 'assets/images/teams/logo/poltava.png',
-    id: 5
-}, {
+},{
     name: 'ТУ МИКОЛАЇВ',
-    logo: 'assets/images/teams/logo/Nikolaev.png',
+    logo: 'assets/images/teams/logo/4_120х137_-Миколаїв-removebg-preview.png',
     id: 6
 }, {
     name: 'ТУ ХМЕЛЬНИЦЬКИЙ',
-    logo: 'assets/images/teams/logo/khmeln.png',
+    logo: 'assets/images/teams/logo/4_120х137_-Хмельницький-removebg-preview.png',
     id: 7
 },];
